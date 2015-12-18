44a271cd80e23563f5f66d8bd2338ec34cb28817
Gilles <ygwane@gmail.com>
2015-12-19 00:46:23 +0100

    Create preconf_centos7.sh

---
+++ preconf_centos7.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Preconf CentOS 7 installed from scratch
+#
+##############
+
+# Get ifconfig
+echo
+echo "Get ifconfig"
+echo
+yum install -y net-tools
+
+# Set hostname
+echo
+echo "Enter hostname to set on this machine:"
+read HOSTNAME
+echo "Define hostname"
+hostnamectl set-hostname ${HOSTNAME}
+
+# Disable firewalld
+echo
+echo "Disable firewalld"
+echo
+systemctl mask firewalld
+systemctl stop firewalld
+
+# Install basics
+echo
+echo "Installing basics"
+echo
+yum install -y htop iotop nload nmap ntp unzip telnet traceroute nc bind-utils rsync mlocate
+
+# Enable NTP
+echo
+echo "Start NTPD"
+echo
+systemctl enable ntpd
+service ntpd restart
+
+# Create bases directories
+echo
+echo "Create /downloads and /scripts directories"
+echo
