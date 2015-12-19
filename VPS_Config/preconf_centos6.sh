6d2acc9890f6fc1431e6287ca2f9811928a58fa2
Gilles <ygwane@gmail.com>
2015-12-19 20:30:38 +0100

    Update preconf_centos6.sh

--- VPS_Config/preconf_centos6.sh
+++ VPS_Config/preconf_centos6.sh
@@ -60,7 +60,7 @@ echo
 echo "Yum update & install Basics RPMs"
 echo
 yum update -y
-yum install -y htop iotop nload ntp vsftpd wget unzip curl telnet traceroute nc bind-utils rsync mlocate vim-enhanced screen
+yum install -y htop iotop nload ntp vsftpd wget unzip curl telnet traceroute nc bind-utils rsync mlocate vim-enhanced screen openssh
 
 ## config ntp
 echo
