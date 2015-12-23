7f3b2008fef07d753177ff2d3bea68fbfcffe837
Gilles <ygwane@gmail.com>
2015-12-23 13:53:22 +0100

    Update preconf_centos7.sh

--- VPS_Config/preconf_centos7.sh
+++ VPS_Config/preconf_centos7.sh
@@ -28,7 +28,7 @@ systemctl stop firewalld
 echo
 echo "Installing basics"
 echo
-yum install -y htop iotop nload nmap ntp unzip telnet traceroute nc bind-utils rsync mlocate screen ncdu
+yum install -y htop iotop nload nmap ntp unzip telnet traceroute nc bind-utils rsync mlocate screen ncdu inxi
 
 # Enable NTP
 echo
