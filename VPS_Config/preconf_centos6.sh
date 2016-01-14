1bc5c35b9e5de01bebf428cf8669928446c7b15b
Gilles <ygwane@gmail.com>
2016-01-14 14:28:54 +0100

    Update preconf_centos6.sh

--- VPS_Config/preconf_centos6.sh
+++ VPS_Config/preconf_centos6.sh
@@ -55,7 +55,7 @@ chkconfig iptables off && service iptables stop
 echo
 echo "Install EPEL & RPMForge"
 echo
-rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 echo
 echo "Yum update & install Basics RPMs"
